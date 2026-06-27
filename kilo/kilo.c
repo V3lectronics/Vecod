4c350da4d2faee27636fd20bc6591a555c7639f5
barbarjan <niszczyciel.agawa@gmail.com>
2026-06-27 13:51:14 +0200

    step 14

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -13,7 +15,9 @@ void enableRawMode() {
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO);
+	raw.c_iflag &= ~(ICRNL | IXON); //disable ctrl S and ctrl Q
+	raw.c_oflag &= ~(OPOST);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //disable ECHO, canonical mode, ctrl v, c and z
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -22,8 +26,13 @@ int main(){
 	enableRawMode();
 
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+	}
+
 	return 0;
 }
-
-
