058c3248b1340bc5af1c81466857988c5fffc824
barbarjan <niszczyciel.agawa@gmail.com>
2026-06-28 09:05:44 +0200

    step 19

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -1,8 +1,13 @@
+/* includes */
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+/* data */
 
 struct termios orig_termios;
 
@@ -11,12 +16,14 @@ void die(const char *s){
 	exit(1);
 }
 
+/* terminal */
+
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 
 void enableRawMode() {
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
@@ -27,15 +34,17 @@ void enableRawMode() {
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VMIN] = 1;
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/* init */
 
 int main(){
 	enableRawMode();
 
 	while (1) {
 		char c = '\0';
-		read(STDIN_FILENO, &c, 1);
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		} else {
