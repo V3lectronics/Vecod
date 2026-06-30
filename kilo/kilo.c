ad9a8ebb1b291e5f977f1d57318da7acb19e8d51
barbarjan <niszczyciel.agawa@gmail.com>
2026-06-30 14:03:53 +0200

    step 21

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -1,11 +1,16 @@
 /* includes */
 
+#include <asm-generic/errno-base.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+/* defines */
+
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /* data */
 
@@ -37,20 +42,36 @@ void enableRawMode() {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey()
+{
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+/* input */
+
+void editorProcessKeypress(void) {
+	char c = editorReadKey();
+
+	switch (c) {
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+
+}
+
 /* init */
 
 int main(){
 	enableRawMode();
 
 	while (1) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == 'q') break;
+		editorProcessKeypress();
 	}
 
 	return 0;
