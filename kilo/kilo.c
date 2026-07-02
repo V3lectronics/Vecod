9756cafbf3fb23930e72df9e3ec1f56afa6c8251
barbarjan <niszczyciel.agawa@gmail.com>
2026-07-02 20:26:58 +0200

    step 31

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -9,6 +9,7 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <ctype.h>
 
 /* defines */
 
@@ -64,11 +65,31 @@ char editorReadKey(){
 	return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+	printf("\r\n");
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1) {
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n",c,c);	
+		}
+	}
+	
+	editorReadKey();
+	
+	return -1;
+}
+
 int getWindowSize(int *rows, int *cols){
 	struct winsize ws;
 
-	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-		return -1;
+	//wtf
+	if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[99C\x1b[99B", 12) != 12) return	-1;
+		return getCursorPosition(rows, cols);
 	} else {
 		*cols = ws.ws_col;
 		*rows = ws.ws_row;
